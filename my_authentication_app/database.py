7c0824ad399c9a25e3b6194c62b15f055f9fe0b0
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-07-07 21:50:43 +0100

    Fix SQLAlchemy dialect for PostgreSQL

--- my_authentication_app/database.py
+++ my_authentication_app/database.py
@@ -12,6 +12,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("No DATABASE_URL set for SQLAlchemy engine")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
