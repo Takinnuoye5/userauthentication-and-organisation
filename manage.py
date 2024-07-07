c15eb7e2c8049fe4394b1835d6c488c6b5bdcb61
AKINNUOYE TIMOTHY <akinnuoyet@gmail.com>
2024-07-07 20:54:28 +0100

    Update alembic configuration for Heroku

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
+# manage.py
 import sys
+import os
+from dotenv import load_dotenv
 from alembic.config import main
 
 if __name__ == "__main__":
+    load_dotenv()
     sys.argv.append("upgrade")
     sys.argv.append("head")
     main()
